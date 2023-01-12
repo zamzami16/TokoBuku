ea76474e2eebc72815dca2f1ecbaf71cbd7d2ae2
zamzami16 <mohammad.yusuf.zamzami-2016@fst.unair.ac.id>
2023-01-12 10:30:15 +0700

    fixed perputaran barang

--- TokoBuku/DbUtility/Report/Transaksi/PerputaranBarang.cs
+++ TokoBuku/DbUtility/Report/Transaksi/PerputaranBarang.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
 using System.Reflection;
 
 namespace TokoBuku.DbUtility.Report.Transaksi
@@ -49,7 +48,7 @@ namespace TokoBuku.DbUtility.Report.Transaksi
 
         internal static DataTable GetPerputaranBarang(DateTime dateMulai, DateTime dateSampai)
         {
-            DataTable data= new DataTable();
+            DataTable data = new DataTable();
             using (var con = ConnectDB.Connetc())
             {
                 var query = "select  period.id_barang, period.nama_barang, period.jumlah, ba.stock from (select dpe.id_barang, dpe.nama_barang, sum(dpe.jumlah) as jumlah from penjualan as p left join (select dpen.id_penjualan, b.id_barang, b.nama_barang, dpen.jumlah from detail_penjualan as dpen left join barang as b on dpen.id_barang=b.id_barang) as dpe on p.id=dpe.id_penjualan where p.tanggal between @dateMulai and @dateSampai group by dpe.id_barang, dpe.nama_barang) as period left join barang as ba on period.id_barang=ba.id_barang;";
@@ -66,37 +65,37 @@ namespace TokoBuku.DbUtility.Report.Transaksi
             return data;
         }
 
-       /* internal static DataTable GetPerputaranBarang(DateTime dateMulai, DateTime dateSampai)
-        {
-            DataTable periode = GetStockPeriode(dateMulai, dateSampai);
-            DataTable prev = GetStockSebelumnya(dateMulai);
-            DataTable data = new DataTable();
-            data.Columns.Add("id_barang", typeof(int));
-            data.Columns.Add("nama_barang", typeof(string));
-            data.Columns.Add("jumlah", typeof(int));
-            data.Columns.Add("JUMLAH_SEBELUM", typeof(int));
-            prev = (prev.Rows.Count < 1) ? onPrevNull(periode) : prev;
-            var results = from table1 in periode.AsEnumerable()
-                          join table2 in prev.AsEnumerable()
-                          on table1.Field<int>("id_barang") equals table2.Field<int>("id_barang")
-                          select new
-                          {
-                              id_barang = table1.Field<int>("id_barang"),
-                              nama_barang = table1.Field<string>("nama_barang"),
-                              jumlah = table1.Field<int>("jumlah"),
-                              JUMLAH_SEBELUM = table2.Field<int>("JUMLAH_SEBELUM")
-                          };
-            foreach (var item in results)
-            {
-                DataRow dr = data.NewRow();
-                dr["nama_barang"] = item.nama_barang;
-                dr["jumlah"] = item.jumlah;
-                dr["JUMLAH_SEBELUM"] = item.JUMLAH_SEBELUM;
-                data.Rows.Add(dr);
-            }
-            return data;
-        }
-*/
+        /* internal static DataTable GetPerputaranBarang(DateTime dateMulai, DateTime dateSampai)
+         {
+             DataTable periode = GetStockPeriode(dateMulai, dateSampai);
+             DataTable prev = GetStockSebelumnya(dateMulai);
+             DataTable data = new DataTable();
+             data.Columns.Add("id_barang", typeof(int));
+             data.Columns.Add("nama_barang", typeof(string));
+             data.Columns.Add("jumlah", typeof(int));
+             data.Columns.Add("JUMLAH_SEBELUM", typeof(int));
+             prev = (prev.Rows.Count < 1) ? onPrevNull(periode) : prev;
+             var results = from table1 in periode.AsEnumerable()
+                           join table2 in prev.AsEnumerable()
+                           on table1.Field<int>("id_barang") equals table2.Field<int>("id_barang")
+                           select new
+                           {
+                               id_barang = table1.Field<int>("id_barang"),
+                               nama_barang = table1.Field<string>("nama_barang"),
+                               jumlah = table1.Field<int>("jumlah"),
+                               JUMLAH_SEBELUM = table2.Field<int>("JUMLAH_SEBELUM")
+                           };
+             foreach (var item in results)
+             {
+                 DataRow dr = data.NewRow();
+                 dr["nama_barang"] = item.nama_barang;
+                 dr["jumlah"] = item.jumlah;
+                 dr["JUMLAH_SEBELUM"] = item.JUMLAH_SEBELUM;
+                 data.Rows.Add(dr);
+             }
+             return data;
+         }
+ */
         private static DataTable onPrevNull(DataTable data)
         {
             DataTable dt = data.Copy();
